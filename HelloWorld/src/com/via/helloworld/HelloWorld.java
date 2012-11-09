39e80155b13845845c4f316880f142eae7215d5e
Ethan Xuan <xuan.yiguang@gmail.com>
2012-11-08 17:36:53 -0800

    Changed output

--- HelloWorld/src/com/via/helloworld/HelloWorld.java
+++ HelloWorld/src/com/via/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("Hello, Dylan!");
+       tv.setText("Hello, Ethan!");
        setContentView(tv);
    }
 }
